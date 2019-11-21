bcb7e3af9926afebc2ff32a72195698ea0c6355f
MouraYuri <mourayuri339@gmail.com>
2019-11-21 20:03:28 -0300

    almost there

--- simple-perceptron/simple-perceptron.py
+++ simple-perceptron/simple-perceptron.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from random import random
+import numpy as np
 def preProcessingData(data):
     for x in range(len(data)):
         data[x] = data[x].split(' ')
@@ -8,14 +9,19 @@ def preProcessingData(data):
     df = pd.DataFrame(data, columns = ['x1', 'x2', 'y'])
     return df
 
-def trainWeights(dataset, threshold, eta):
-    '''
-    df.columns => return number of columns
-
-    '''
-
+def train(dataset, threshold, eta, eras):
+    #assigning random values to weights and assigning value 1 to theta
+    weightsVector, theta = np.array([]), 1
+    for x in range(len(dataset.columns)-1):
+        weightsVector = np.append(weightsVector, [random()])
+    
+    #training start
+    for era in range(eras):
+        for i in range(len(dataset)):
+            y = np.inner(dataset.iloc[i, :len(dataset.columns)-1], weightsVector) #estimated y
+            
 
 
 dataset = preProcessingData(open('./simple-perceptron/dataset.txt', 'r').readlines())
-print(dataset)
+train(dataset, 0.5, 0.1, 1)
 
