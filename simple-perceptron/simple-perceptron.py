30f8b16e9e6e733600d13f94e27ced2ef3bb0488
MouraYuri <mourayuri339@gmail.com>
2019-11-28 20:49:24 -0300

    oh

--- simple-perceptron/simple-perceptron.py
+++ simple-perceptron/simple-perceptron.py
@@ -9,26 +9,35 @@ def preProcessingData(data):
     df = pd.DataFrame(data, columns = ['x1', 'x2', 'y'])
     return df
 
-def train(dataset, threshold, eta, eras):
+def f(net):
+    if (net>=0.5) : return 1
+    return 0
+
+def test(weights):
+    xVector = (np.array([1,1])).transpose()
+    print(f(np.inner(xVector, weights)))
+
+def train(dataset, era, eta):
     #assigning random values to weights and assigning value 1 to theta
     weightsVector, theta = np.array([]), 1
     for x in range(len(dataset.columns)-1):
         weightsVector = np.append(weightsVector, [random()])
     
     #training start
-    for era in range(eras):
+    for t in range(era):
         for i in range(len(dataset)):
-            yHat = (np.inner(dataset.iloc[i, :len(dataset.columns)-1], weightsVector) + theta) #estimated y | Provável bucho aqui ein
-            E = dataset.iloc[i, len(dataset.columns)-1]-yHat
-            
+            yHat = f((np.inner(dataset.iloc[i, :len(dataset.columns)-1], weightsVector) + theta)) #estimated y | Provável bucho aqui ein
+            E = dataset.iloc[i, len(dataset.columns)-1]-yHat #error
+
             #Updating weights and theta
             for x in range(len(weightsVector)):
                 weightsVector[x] = weightsVector[x] -  eta*(2*E*(-(dataset.iloc[i, x])))
             theta = theta - eta*(-2*E)
-        
+
         #Shuffling the dataset
         dataset.sample(frac=1)
-    print(weightsVector)
+    return weightsVector
 dataset = preProcessingData(open('./simple-perceptron/dataset.txt', 'r').readlines())
-train(dataset, 0.5, 0.1, 10)
+weights = (train(dataset, 10, 0.1))
+test(weights)
 
