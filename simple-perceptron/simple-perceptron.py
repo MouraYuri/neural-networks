7aae51cf7331b0f02612d962f5b88b1c581851ad
MouraYuri <mourayuri339@gmail.com>
2019-12-25 16:23:39 -0300

    lets bora

--- simple-perceptron/simple-perceptron.py
+++ simple-perceptron/simple-perceptron.py
@@ -9,35 +9,5 @@ def preProcessingData(data):
     df = pd.DataFrame(data, columns = ['x1', 'x2', 'y'])
     return df
 
-def f(net):
-    if (net>=0.5) : return 1
-    return 0
-
-def test(weights):
-    xVector = (np.array([1,1])).transpose()
-    print(f(np.inner(xVector, weights)))
-
-def train(dataset, era, eta):
-    #assigning random values to weights and assigning value 1 to theta
-    weightsVector, theta = np.array([]), 1
-    for x in range(len(dataset.columns)-1):
-        weightsVector = np.append(weightsVector, [random()])
-    
-    #training start
-    for t in range(era):
-        for i in range(len(dataset)):
-            yHat = f((np.inner(dataset.iloc[i, :len(dataset.columns)-1], weightsVector) + theta)) #estimated y | Provável bucho aqui ein
-            E = dataset.iloc[i, len(dataset.columns)-1]-yHat #error
-
-            #Updating weights and theta
-            for x in range(len(weightsVector)):
-                weightsVector[x] = weightsVector[x] -  eta*(2*E*(-(dataset.iloc[i, x])))
-            theta = theta - eta*(-2*E)
-
-        #Shuffling the dataset
-        dataset.sample(frac=1)
-    return weightsVector
-dataset = preProcessingData(open('./simple-perceptron/dataset.txt', 'r').readlines())
-weights = (train(dataset, 10, 0.1))
-test(weights)
-
+dataset = preProcessingData(open('./simple-perceptron/dataset2', 'r').readlines())
+print(dataset)
