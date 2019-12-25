420ac940a9df991a4341b883dfb755bbc138b81d
MouraYuri <mourayuri339@gmail.com>
2019-12-25 19:53:05 -0300

    jsajd

--- simple-perceptron/simple-perceptron.py
+++ simple-perceptron/simple-perceptron.py
@@ -32,23 +32,27 @@ class Percetron:
         return data
 
     def train(self):
-        for _ in range(self.eras):
-            totalError = 0
+        while(True):
+            totalError=0
             for x in range(len(self.dataFrame)):
                 row = (self.dataFrame.iloc[x, :-1]).to_numpy()
-                row = np.append(row, 1)
+                row = np.append(row, -1)
                 yHat = self.f(np.inner(row, self.weights))
                 e = int(self.dataFrame.iloc[x, -1:]) - yHat
                 if e != 0:
-                    totalError +=1
+                    totalError += 1
                     for y in range(len(self.weights)):
                         self.weights[y] = self.weights[y] + self.eta*e*(row[y])
+            hits = len(self.dataFrame) - totalError
+            hitsPercentage = hits/len(self.dataFrame)
+            print('hits => {}\nhitsPercentage => {}'.format(hits, hitsPercentage))
+            if totalError == 0: break
             self.dataFrame = self.dataFrame.sample(frac=1)
 
     def test(self, values):
         return self.f(np.inner(self.weights, values))
 
 data = open('./simple-perceptron/dataset2', 'r')
-p = Percetron(data, 4, 0.1, 30)
+p = Percetron(data, 4, 0.1, 20)
 print(p.test([5.7, 3.8, 1.7, 0.3, 1]))
 print(p.test([7.9, 3.8, 6.4, 2.0, 1]))
