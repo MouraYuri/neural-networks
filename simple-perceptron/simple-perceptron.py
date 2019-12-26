84c4dd4561e4badcfed9f339e3f3a4165dd13e30
MouraYuri <mourayuri339@gmail.com>
2019-12-26 12:29:14 -0300

    asdsa

--- simple-perceptron/simple-perceptron.py
+++ simple-perceptron/simple-perceptron.py
@@ -14,11 +14,10 @@ import numpy as np
 
 class Percetron:
     
-    def __init__(self, data, n, eta, eras):
+    def __init__(self, data, n, eta):
         self.dataFrame = self.preProcessingData(data)
         self.weights = np.random.rand(1, 5)
         self.eta = eta
-        self.eras = eras
         self.train()
 
     def f(self, x):
@@ -32,27 +31,41 @@ class Percetron:
         return data
 
     def train(self):
-        while(True):
+        for _ in range(100):
+        #while(True):
             totalError=0
             for x in range(len(self.dataFrame)):
                 row = (self.dataFrame.iloc[x, :-1]).to_numpy()
-                row = np.append(row, -1)
+                row = np.append(row, 1)
                 yHat = self.f(np.inner(row, self.weights))
                 e = int(self.dataFrame.iloc[x, -1:]) - yHat
-                if e != 0:
-                    totalError += 1
-                    for y in range(len(self.weights)):
-                        self.weights[y] = self.weights[y] + self.eta*e*(row[y])
+                if e != 0: totalError += 1
+                for y in range(len(self.weights)):
+                    self.weights[y] += self.eta*e*(row[y])
             hits = len(self.dataFrame) - totalError
             hitsPercentage = hits/len(self.dataFrame)
             print('hits => {}\nhitsPercentage => {}'.format(hits, hitsPercentage))
+            print(self.weights)
             if totalError == 0: break
             self.dataFrame = self.dataFrame.sample(frac=1)
 
     def test(self, values):
         return self.f(np.inner(self.weights, values))
 
+    def test_dataset(self, data):
+        dfToTest, totalError = (self.preProcessingData(data)).sample(frac=1), 0
+        for x in range(len(dfToTest)):
+            row = (dfToTest.iloc[x, :-1]).to_numpy()
+            row = np.append(row, 1)
+            yHat = self.test(row)
+            e = int(dfToTest.iloc[x, -1:]) - yHat
+            if (e!=0): totalError+=1
+        hits = len(dfToTest) - totalError
+        hitsPercentage = hits/len(dfToTest)
+        print('hits => {}\nhitsPercentage => {}'.format(hits, hitsPercentage))
+
 data = open('./simple-perceptron/dataset2', 'r')
-p = Percetron(data, 4, 0.1, 20)
-print(p.test([5.7, 3.8, 1.7, 0.3, 1]))
-print(p.test([7.9, 3.8, 6.4, 2.0, 1]))
+datatotest = open('./simple-perceptron/datatotest', 'r')
+p = Percetron(data, 4, 0.01)
+print('===================================================')
+p.test_dataset(datatotest)
