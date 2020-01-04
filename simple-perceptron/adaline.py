69b449b16dc4efcb3b6be00bea417c56aff0d2a3
MouraYuri <mourayuri339@gmail.com>
2020-01-04 16:13:15 -0300

    asjd

--- simple-perceptron/adaline.py
+++ simple-perceptron/adaline.py
@@ -24,23 +24,40 @@ class Adaline :
         if x >=0: return 1
         else: return -1
 
-    def sse(self):
+    def sse(self): #Sum of Squared Errors
+        ctrl = 0
+        for i in range(len(self.data)):
+            row, y = np.append(-1, (self.data.iloc[i][:-1]).to_numpy()), self.data.iloc[i][-1]
+            linearOutput = np.inner(row, self.weights)
+            ctrl += ((y - linearOutput)**2)/2
+        return ctrl
+        
+
         
 
     def train(self):
-        for _ in range(10):
-        #for _ in range(self.n_iter):
-            sse, ctrl = 0, 0 #(SSE == Sum of Squared Errors)
-            for Xj in range(len(self.data.columns) - 1):
+        for _ in range(self.n_iter):
+            print('========================================')
+            self.data = self.data.sample(frac=1) #shuffling the dataset
+            weights = self.weights
+            
+            #Updating weights
+            for w in range(len(self.weights)):
+                deltaW = 0
                 for i in range(len(self.data)):
-                    row = np.append(-1, (self.data.iloc[i][:-1]).to_numpy())
-                    #row = np.append(-1, row)
-                    #sse += (self.data.iloc[x][-1])
-                    ctrl += (self.data.iloc[i][-1] - np.inner(row, self.weights))*self.data.iloc[i][Xj]
-                    print(ctrl)
+                    row = np.append(-1, self.data.iloc[i][:-1])
+                    linearOutput = np.inner(row, weights) 
+                    y = self.data.iloc[i][-1]
+                    deltaW += (y - linearOutput)*row[w]
+                self.weights[w] += self.eta*deltaW
+                print('sse => ', self.sse())
+        print('weights final => ',self.weights)
+        print('sse final => ', self.sse())
+        
+
 
 
 
 data = open('./simple-perceptron/dataset2', 'r')
 datatotest = open('./simple-perceptron/datatotest', 'r')
-a = Adaline(data, 0.01, 50)
+a = Adaline(data, 0.1, 10)
